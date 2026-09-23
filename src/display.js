02776f6d6321406a2e0725e73131c80c04f8b819
Ani <205335742+Ani0o@users.noreply.github.com>
2026-09-24 00:10:52 +0530

    Create the display module
    This module provides functions to render the projects and todos dom
    elements on the webpage.

---
+++ src/display.js
@@ -0,0 +1,204 @@
+import { projects } from "./projectManager";
+import editIcon from "./icons/edit.svg";
+import deleteIcon from "./icons/delete.svg";
+import { formatDistanceToNow } from "date-fns";
+
+const projectList = document.querySelector('.side-nav-project-list');
+const selectedProjectHeading = document.querySelector('.selected-project');
+
+function displayProjects(selectedProject) {
+    projectList.replaceChildren();
+    projects.forEach((project) => {
+        const p = document.createElement('p');
+
+        p.textContent = project.title;
+        p.classList.add('project');
+        if (project.id === selectedProject.id) p.classList.add('active-project');
+        p.setAttribute('data-id', project.id);
+
+        projectList.appendChild('p');
+    });
+    selectedProjectHeading.textContent = `# ${selectedProject.title}`;
+}
+
+const todoList = document.querySelector('.todo-section-content');
+
+function displayTodos(selectedProject) {
+    todoList.replaceChildren();
+    selectedProject.todos.forEach((todo) => {
+        const todoWrapper = document.createElement('div');
+        const todoHeader = document.createElement('div');
+        const todoHeaderLeftWrapper = document.createElement('div');
+        const checkbox = document.createElement('div');
+        const todoTitle = document.createElement('p');
+        const todoHeaderRightWrapper = document.createElement('div');
+        const todoPriority = document.createElement('p');
+        const todoDueDate = document.createElement('p');
+        const todoMenu = document.createElement('div');
+        const todoContentWrapper = document.createElement('div');
+        const todoContent = document.createElement('div');
+        const todoDescription = document.createElement('p');
+        const todoContentRightWrapper = document.createElement('div');
+        const editPriorityButton = document.createElement('button');
+        const editPriorityIcon = document.createElement('img');
+        const editDueDateButton = document.createElement('button');
+        const editDueDateIcon = document.createElement('img');
+        const deleteTodoButton = document.createElement('button');
+        const deleteTodoIcon = document.createElement('img');
+        const editPriorityDialog = document.createElement('dialog');
+        const editPriorityForm = document.createElement('form');
+        const editPriorityFormInputWrapper = document.createElement('div');
+        const editPriorityFormLabel = document.createElement('label');
+        const editPriorityFormSelect = document.createElement('select');
+        const editPriorityFormSelectOption1 = document.createElement('option');
+        const editPriorityFormSelectOption2 = document.createElement('option');
+        const editPriorityFormSelectOption3 = document.createElement('option');
+        const editPriorityFormButtonsWrapper = document.createElement('div');
+        const editPriorityFormCancelButton = document.createElement('button');
+        const editPriorityFormSubmitButton = document.createElement('button');
+        const editDueDateDialog = document.createElement('dialog');
+        const editDueDateForm = document.createElement('form');
+        const editDueDateFormInputWrapper = document.createElement('div');
+        const editDueDateFormLabel = document.createElement('label');
+        const editDueDateFormInput = document.createElement('input');
+        const editDueDateFormButtonsWrapper = document.createElement('div');
+        const editDueDateFormCancelButton = document.createElement('button');
+        const editDueDateFormSubmitButton = document.createElement('button');
+
+        todoWrapper.setAttribute('data-id', todo.id);
+        todoWrapper.classList.add('todo');
+        if (todo.complete === "true") todoWrapper.classList.add('todo-complete');
+
+        todoHeader.classList.add('todo-header');
+        todoHeaderLeftWrapper.classList.add('todo-header-left-wrapper');
+        checkbox.classList.add('checkbox');
+        checkbox.setAttribute('data-id', todo.id);
+        todoTitle.classList.add('todo-title');
+        todoTitle.textContent = todo.title;
+
+        todoHeaderLeftWrapper.appendChild(checkbox);
+        todoHeaderLeftWrapper.appendChild(todoTitle);
+        
+        todoHeaderRightWrapper.classList.add('todo-header-right-wrapper');
+        todoPriority.classList.add('todo-priority');
+
+        if (todo.priority === "1") todoPriority.textContent = "Low Priority";
+        else if (todo.priority === "2") todoPriority.textContent = "Medium Priority";
+        else todoPriority.textContent = "High Priority";
+
+        todoDueDate.classList.add('todo-duedate');
+
+        let [year, month, day] = todo.date.split('-');
+        month = Number(month) - 1;
+        todoDueDate.textContent = `Due ${formatDistanceToNow(new Date(year, month, day), { addSuffix: true, includeSeconds: true })}`;
+
+        todoMenu.classList.add('todo-menu');
+
+        todoHeaderRightWrapper.appendChild(todoPriority);
+        todoHeaderRightWrapper.appendChild(todoDueDate);
+        todoHeaderRightWrapper.appendChild(todoMenu);
+
+        todoHeader.appendChild(todoHeaderLeftWrapper);
+        todoHeader.appendChild(todoHeaderRightWrapper);
+
+        todoContentWrapper.classList.add('todo-content-wrapper');
+        todoContent.classList.add('todo-content');
+        todoDescription.classList.add('todo-description');
+        todoDescription.textContent = todo.description;
+        todoContentRightWrapper.classList.add('todo-content-right-wrapper');
+        editPriorityButton.classList.add('edit-priority-button');
+        editPriorityButton.setAttribute('data-id', todo.id);
+        editPriorityButton.textContent = "Edit Priority";
+        editPriorityIcon.src = editIcon;
+        editPriorityButton.appendChild(editPriorityIcon);
+        editDueDateButton.classList.add('edit-duedate-button');
+        editDueDateButton.setAttribute('data-id', todo.id);
+        editDueDateButton.textContent = "Edit Due Date";
+        editDueDateIcon.src = editIcon;
+        editDueDateButton.appendChild(editDueDateIcon);
+        deleteTodoButton.classList.add('delete-todo-button');
+        deleteTodoButton.setAttribute('data-id', todo.id);
+        deleteTodoButton.textContent = "Delete";
+        deleteTodoIcon.src = deleteIcon;
+        deleteTodoButton.appendChild(deleteTodoIcon);
+
+        editPriorityDialog.classList.add('edit-priority-dialog');
+        editPriorityFormLabel.htmlFor = 'edit-form-priority';
+        editPriorityFormSelect.id = 'edit-form-priority';
+        editPriorityFormSelect.name = 'priority';
+        editPriorityFormSelectOption1.value = '1';
+        editPriorityFormSelectOption1.textContent = "Low";
+        editPriorityFormSelectOption2.value = '2';
+        editPriorityFormSelectOption2.textContent = "Medium";
+        editPriorityFormSelectOption3.value = '3';
+        editPriorityFormSelectOption3.textContent = "High";
+        editPriorityFormSelect.appendChild(editPriorityFormSelectOption1);
+        editPriorityFormSelect.appendChild(editPriorityFormSelectOption2);
+        editPriorityFormSelect.appendChild(editPriorityFormSelectOption3);
+
+        editPriorityFormInputWrapper.appendChild(editPriorityFormLabel);
+        editPriorityFormInputWrapper.appendChild(editPriorityFormSelect);
+
+        editPriorityFormCancelButton.classList.add('edit-priority-form-cancel-button');
+        editPriorityFormCancelButton.type = 'button';
+        editPriorityFormCancelButton.value = 'cancel';
+        editPriorityFormCancelButton.formNoValidate = true;
+        editPriorityFormCancelButton.textContent = "Cancel";
+        editPriorityFormSubmitButton.classList.add('edit-priority-form-submit-button');
+        editPriorityFormSubmitButton.type = 'submit';
+        editPriorityFormSubmitButton.value = 'submit';
+        editPriorityFormSubmitButton.textContent = "Add";
+
+        editPriorityFormButtonsWrapper.appendChild(editPriorityFormCancelButton);
+        editPriorityFormButtonsWrapper.appendChild(editPriorityFormSubmitButton);
+
+        editPriorityForm.appendChild(editPriorityFormInputWrapper);
+        editPriorityForm.appendChild(editPriorityFormButtonsWrapper);
+        editPriorityDialog.appendChild(editPriorityForm);
+
+        editDueDateDialog.classList.add('edit-duedate-dialog');
+        editDueDateFormLabel.htmlFor = 'edit-form-duedate';
+        editDueDateFormLabel.textContent = "Due Date:";
+        editDueDateFormInput.id = 'edit-form-duedate';
+        editDueDateFormInput.type = 'date';
+        editDueDateFormInput.name = 'duedate';
+        editDueDateFormInput.required = true;
+
+        editDueDateFormInputWrapper.appendChild(editDueDateFormLabel);
+        editDueDateFormInputWrapper.appendChild(editDueDateFormInput);
+
+        editDueDateFormCancelButton.classList.add('edit-duedate-form-cancel-button');
+        editDueDateFormCancelButton.type = 'button';
+        editDueDateFormCancelButton.value = 'cancel';
+        editDueDateFormCancelButton.formNoValidate = true;
+        editDueDateFormCancelButton.textContent = "Cancel";
+        editDueDateFormSubmitButton.classList.add('edit-duedate-form-submit-button');
+        editDueDateFormSubmitButton.type = 'submit';
+        editDueDateFormSubmitButton.value = 'submit';
+        editDueDateFormSubmitButton.textContent = "Add";
+
+        editDueDateFormButtonsWrapper.appendChild(editDueDateFormCancelButton);
+        editDueDateFormButtonsWrapper.appendChild(editDueDateFormSubmitButton);
+
+        editDueDateForm.appendChild(editDueDateFormInputWrapper);
+        editDueDateForm.appendChild(editDueDateFormButtonsWrapper);
+        editDueDateDialog.appendChild(editDueDateForm);
+
+        todoContentRightWrapper.appendChild(editPriorityButton);
+        todoContentRightWrapper.appendChild(editDueDateButton);
+        todoContentRightWrapper.appendChild(deleteTodoButton);
+        todoContentRightWrapper.appendChild(editPriorityDialog);
+        todoContentRightWrapper.appendChild(editDueDateDialog);
+
+        todoContent.appendChild(todoDescription);
+        todoContent.appendChild(todoContentRightWrapper);
+        todoContentWrapper.appendChild(todoContent);
+
+        todoWrapper.appendChild(todoHeader);
+        todoWrapper.appendChild(todoContentWrapper);
+
+        todoList.appendChild(todoWrapper);
+    });
+}
+
+export { displayProjects, displayTodos };
